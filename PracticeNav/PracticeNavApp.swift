2201b5b257f0a04a8dbcbda23a537b2203bc54f8
DaianaM <daiana.m.0708@gmail.com>
2023-04-26 16:22:39 -0400

    Initial Commit

---
+++ PracticeNav/PracticeNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeNavApp.swift
+//  PracticeNav
+//
+//  Created by scholar on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
